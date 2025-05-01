2b3381058aaeb1bacaf8961b837bc1232658238e
RandyNixon <79169426+nixonrs-bucs@users.noreply.github.com>
2025-04-30 23:45:45 -0400

    Redo  on the project

---
+++ CSCI3110FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSCI3110FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
